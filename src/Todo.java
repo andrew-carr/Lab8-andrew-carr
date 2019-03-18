5a176ab01a62c95ae5cede8dac02a35ff7e20194
Andrew Carr <andrew.s.carr@ou.edu>
2019-03-18 16:52:41 -0500

    Dropping files

---
+++ src/Todo.java
@@ -0,0 +1,15 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-03-18
+OU ID_______________: 113404790
+OU EMAIL____________: andrew.s.carr@ou.edu
+
+Tasks:
+1. 
+
+Estimated Time (minutes):
+1. 
+
+Actual Time (minutes):
+1. 
+*/}
